cef77df5606973a3d695c9a70ccf6f4c80c74d10
TheGreatBoogie <100144995+TheGreatBoogie@users.noreply.github.com>
2024-02-26 20:51:29 -0500

    Base prototype

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Debug.Log("Clicked!!!");
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIController : MonoBehaviour
+{
+    public void OnEnable()
+    {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        
+        
+        Button buttonOne = root.Q<Button>("button-one");
+        Button buttonTwo = root.Q<Button>("button-two");
+        Button buttonThree = root.Q<Button>("button-three");
+        Button buttonFour = root.Q<Button>("button-four");
+        Button buttonFive = root.Q<Button>("button-five");
+        Button buttonSix = root.Q<Button>("button-six");
+        Button buttonSeven = root.Q<Button>("button-seven");
+        Button buttonHeight = root.Q<Button>("button-height");
+        Button buttonNine = root.Q<Button>("button-nine");
+        Button buttonZero = root.Q<Button>("button-zero");
+        Button buttonClear = root.Q<Button>("button-clear");
+        Button buttonOk = root.Q<Button>("button-ok");
+        
+        
+    }
+}
